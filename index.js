b907152b5325d40613436e84335f27595f83c152
maheshsuthar05 <maheshsuthar051196@gmail.com>
2021-11-25 18:51:37 +0530

    reduced use of Number method

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ checkStr(str)
 function checkStr(str){
     var sum=0
     for(var i=0; i<str.length; i++){
-        if(Number(str[i])>=0 && Number(str[i]<=9)){
-            var char = Number(str[i])
+        var char = Number(str[i])
+        if(char>=0 && char<=9){
             sum += char
         }
     }
